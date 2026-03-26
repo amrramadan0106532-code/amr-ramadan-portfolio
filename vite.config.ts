469b100ec50a2b74d0e499a64dd6e66c2f1bd56a
AmrNeqabty <amr.ramadan@neqabty.com>
2026-03-26 12:56:36 +0200

    index file

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/amr-ramadan-portfolio/', // THIS IS CRUCIAL
-  plugins: [react(), tailwindcss()],
+  base: '/amr-ramadan-portfolio/',
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
   },
 });
